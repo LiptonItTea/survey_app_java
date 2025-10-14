c1bf1d9d795c12b1246f8c81067b50557c11c377
LiptonItTea <stormtrooper.145145@gmail.com>
2025-10-14 14:41:58 +0300

    testing sign in

--- src/main/java/org/liptonit/Vars.java
+++ src/main/java/org/liptonit/Vars.java
@@ -8,10 +8,10 @@ import org.liptonit.entity.*;
 public class Vars {
     private static final Database db = new InMemoryDatabase();
 
-    public static Repository<Answer> answerRepository = new Repository<>(db, Answer.class);
-    public static Repository<CompletedSurvey> completedSurveyRepository = new Repository<>(db, CompletedSurvey.class);
-    public static Repository<QuestionAnswer> questionAnswerRepository = new Repository<>(db, QuestionAnswer.class);
-    public static Repository<Question> questionRepository = new Repository<>(db, Question.class);
-    public static Repository<Survey> surveyRepository = new Repository<>(db, Survey.class);
-    public static Repository<User> userRepository = new Repository<>(db, User.class);
+    public static final Repository<Answer> answerRepository = new Repository<>(db, Answer.class);
+    public static final Repository<CompletedSurvey> completedSurveyRepository = new Repository<>(db, CompletedSurvey.class);
+    public static final Repository<QuestionAnswer> questionAnswerRepository = new Repository<>(db, QuestionAnswer.class);
+    public static final Repository<Question> questionRepository = new Repository<>(db, Question.class);
+    public static final Repository<Survey> surveyRepository = new Repository<>(db, Survey.class);
+    public static final Repository<User> userRepository = new Repository<>(db, User.class);
 }
--- src/test/java/org/liptonit/ScenariosTest.java
+++ src/test/java/org/liptonit/ScenariosTest.java
@@ -3,19 +3,45 @@ package org.liptonit;
 import org.junit.jupiter.api.Test;
 import org.liptonit.entity.User;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ScenariosTest {
 
     @Test
-    void example() {
-        List<User> all = Vars.userRepository.readEntities(u -> true);
+    void testSignInSuccessful() {
+        // Arrange
+        String nickname = "testuser_success";
+        String password = "password";
+        Scenarios.signUp(nickname, "test_success@example.com", password);
 
-        List<User> truth = new ArrayList<>();
-        truth.add(new User(0, "amogus", "amogus@mail.ru", LocalDate.now(), "amoguspassword"));
+        // Act
+        User user = Scenarios.signIn(nickname, password);
+
+        // Assert
+        assertNotNull(user);
+        assertEquals(nickname, user.getNickname());
+    }
+
+    @Test
+    void testSignInWrongPassword() {
+        // Arrange
+        String nickname = "testuser_wrongpass";
+        String password = "password";
+        Scenarios.signUp(nickname, "test_wrongpass@example.com", password);
+
+        // Act
+        User user = Scenarios.signIn(nickname, "wrongpassword");
+
+        // Assert
+        assertNull(user);
+    }
+
+    @Test
+    void testSignInNonExistentUser() {
+        // Act
+        User user = Scenarios.signIn("nonexistentuser", "password");
+
+        // Assert
+        assertNull(user);
     }
 }
