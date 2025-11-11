cb7c87d90ccd82b859156c93e608781ce53e57a5
LiptonItTea <stormtrooper.145145@gmail.com>
2025-11-12 00:13:35 +0300

    h2 support

--- src/main/java/org/liptonit/db/JDBC.java
+++ src/main/java/org/liptonit/db/JDBC.java
@@ -6,16 +6,20 @@ import java.sql.SQLException;
 
 public class JDBC {
     private static Connection connection;
-    private static final String url = "jdbc:postgresql://localhost:5432/surveys";
-    private static final String user = "postgres";
-    private static final String password = "meow";
+//    private static final String url = "jdbc:postgresql://localhost:5432/surveys";
+    private static final String url = "jdbc:h2:~/test";
+//    private static final String user = "postgres";
+private static final String user = "sa";
+//    private static final String password = "meow";
+private static final String password = "";
 
     public static Connection getConnection() {
         try {
+            Class.forName ("org.h2.Driver");
             if (connection == null)
                 connection = DriverManager.getConnection(url, user, password);
         }
-        catch (SQLException e) {
+        catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
 
